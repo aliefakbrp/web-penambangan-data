067e01e21e92549ef1c84871f503b56677767462
08-Ahlaqul-Karimah <83933498+08-Ahlaqul-Karimah@users.noreply.github.com>
2022-11-19 19:00:55 +0700

    Update knaps.py

--- knaps.py
+++ knaps.py
@@ -23,7 +23,7 @@ tab1, tab2, tab3, tab4 = st.tabs(["Import Data", "Preprocessing", "Modelling", "
 
 with tab1:
     st.write("Import Data")
-    data = pd.read_csv("https://raw.githubusercontent.com/08-Ahlaqul-Karimah/machine-Learning/main/mushrooms.csv.csv")
+    data = pd.read_csv("https://raw.githubusercontent.com/08-Ahlaqul-Karimah/machine-Learning/main/credit_score%20(1).csv")
     st.dataframe(data)
 
 with tab2:
